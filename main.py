339b7534fa7e661f57f87fbfd81ce3db1ed61f4d
dragosJJJ <dragos_juganariu@yahoo.com>
2023-10-18 23:41:17 +0300

    initial

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+from twilio.rest import Client
+
+api_key = "1460a326b06906f175e99b679563040a"
+account_sid = 'AC2117b51f874d6c08512bcecf36601b40'
+auth_token = '352aea47b92be73ce72f03d691b8b6ea'
+
+client= Client(account_sid, auth_token)
+
+weather_params = {
+    "lat": 44.318378,
+    "lon": 23.796400,
+    "appid": api_key
+
+}
+
+response = requests.get(url=f"https://api.openweathermap.org/data/2.5/weather", params=weather_params)
+response.raise_for_status()
+weather_code = response.json()["weather"][0]["id"]
+
+if 500<= weather_code <=531:
+
+    message = client.messages.create(
+        from_='+17817677738',
+        body=r"It's raining outside! You should bring an umbrella.",
+        to = '+40761623757'
+    )
+
+    print(message.sid)
+else:
+    message = client.messages.create(
+        from_='+17817677738',
+        body=r"Clear sky!",
+        to='+40761623757'
+    )
+
+    print(message.sid)
